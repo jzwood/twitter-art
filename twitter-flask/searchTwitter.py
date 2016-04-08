ae9ca181944913a3b920485b4451205c5badc019
Jake Wood <jake.wood@sfm1pcg8wml.na.akqa.local>
2016-04-08 13:36:45 -0700

    added include and extend jinja

---
+++ twitter-flask/searchTwitter.py
@@ -0,0 +1,41 @@
+from TwitterAPI import TwitterAPI
+#import datetime
+from datetime import datetime
+import re
+
+# feed this 'created_at' dict value
+def cleanDateString(datestring):
+    clean_timestamp = datetime.strptime(datestring, '%a %b %d %H:%M:%S +0000 %Y')
+    #convert to am/pm format for easy reading
+    formatted_date =  ' ' + datetime.strftime(clean_timestamp,'%I:%M:%S%p %a %b %d %Y')
+    formatted_date = re.sub(r' 0([0-9])|([0-9])0 ',r' \1',formatted_date).strip()
+    return formatted_date
+
+def authenticate():
+    # Variables that contains the user credentials to access Twitter API
+    ACCESS_TOKEN = '701456120825536512-VjgPUhj4U67XsOlnMruognOasLkBvyj'
+    ACCESS_SECRET = 'uLF7wBCOrwVmtqzIbwzjKWoUCNjPsYhuRGGAJveAcicHq'
+    CONSUMER_KEY = 'KGiy3gzu9J70N8gFiNDTCHWMN'
+    CONSUMER_SECRET = 'PBK83DQDEKdjqiiohfyIucaSL4mz2WIHxVxkcfruEOOwgXveXg'
+    #get twitter api obj
+    api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+    return api
+
+#e.g. getJSONfromQuery('"sing out"', authenticate())
+def getJSONfromQuery(q_str,api,resp_num):
+    # r = api.request('search/tweets', {'q':'list:NASA/astronauts-in-space-now', 'lang':'en', 'count':3, 'result_type': 'recent'})
+    r = api.request('search/tweets', {'q':q_str, 'lang':'en', 'count':resp_num, 'result_type': 'mixed'})
+    #
+    items = [a for a in r]
+    #
+    handle = lambda i: '@' + items[int(i)]['user']['screen_name']
+    timestamp = lambda i: cleanDateString(items[int(i)]['created_at'])
+    text = lambda i: items[int(i)]['text']
+    #
+    data = []
+    #
+    for i,entry in enumerate(items):
+        ha,ti,tx = handle(i), timestamp(i), text(i)
+        data.append({'handle':ha, 'timestamp':ti, "text": tx})
+    #
+    return data
--- twitter-flask/twit-app.py
+++ twitter-flask/twit-app.py
@@ -3,33 +3,39 @@
 from flask import Flask, render_template
 from flask.ext.scss import Scss
 import sqlite3
+import searchTwitter as tw
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 Scss(app, static_dir='static', asset_dir='assets')
 
-@app.route('/test')
+@app.route('/')
 def index():
-    conn = sqlite3.connect('test_database.db')
-    c = conn.cursor()
+    # conn = sqlite3.connect('test_database.db')
+    # c = conn.cursor()
+    #
+    # data_stream = c.execute('''SELECT * FROM records''')
+    #
+    # x    = []
+    # down = []
+    # up   = []
+    # data = []
+    #
+    # for d in data_stream:
+    #     x.append(d[0])
+    #     down.append(d[1])
+    #     up.append(d[2])
+    #     data.append([format_datetime(d[0]), d[1], d[2]])
+    api = tw.authenticate()
+    results = tw.getJSONfromQuery('"you know that there are"',api,3)
+    # results = [{'text': 'Citizens should be free to speak without having to consult the @Tx_Ethics Comm or an election lawyer or file mind-boggling forms.', 'timestamp': '7:11:22PM Fri Apr 8 2016', 'handle': '@TweetTonyMac'},
+    #            {'text': "RT @PondsPH: Visit https://t.co/cfq3E29rrz to get your FREE beauty gift. Be one of the Philippines' most beautiful faces, with Pond's. @hel…", 'timestamp': '7:11:20PM Fri Apr 8 2016', 'handle': '@xilcabels'}]
+    print(results)
 
-    data_stream = c.execute('''SELECT * FROM records''')
+    return render_template('index.html', data=results)
 
-    x    = []
-    down = []
-    up   = []
-    data = []
-
-    for d in data_stream:
-        x.append(d[0])
-        down.append(d[1])
-        up.append(d[2])
-        data.append([format_datetime(d[0]), d[1], d[2]])
-
-    return render_template('index.html', domain=x, down=down, up=up, data=data)
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/home/')
+@app.route('/home/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
