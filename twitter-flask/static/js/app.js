1b8b8ae7859df279fce1ac0fa30fc490cb8ccb60
Jake Wood <jzwood14@yahoo.com>
2016-04-10 23:00:11 -0700

    added more backend tuning, and li animation with gs

--- twitter-flask/static/js/app.js
+++ twitter-flask/static/js/app.js
@@ -1,1 +1,32 @@
+var player, tl, tweets;
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '378',
+    width: '672',
+    videoId: 'gTziWaeMYgM?rel=0',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
 
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  // tl = new TimelineLite();
+  // tweets = document.querySelectorAll('.tweet-content');
+  // console.log("Buffered and ready to play!");
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+function onPlayerStateChange(event) {
+  var ps = player.getPlayerState();
+  console.log(player.getCurrentTime(),player.getPlayerState());
+  // if(ps === 1){
+  //   tl.to(tweets, 3, {opacity:0});
+  // }
+}
--- twitter-flask/twit-app.py
+++ twitter-flask/twit-app.py
@@ -2,8 +2,10 @@
 
 from flask import Flask, render_template
 from flask.ext.scss import Scss
-import sqlite3
-import searchTwitter as tw
+import json, datetime
+
+# import sqlite3
+# import searchTwitter as tw
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,11 +18,18 @@ def index(info=None):
     # api = tw.authenticate()
     # results = tw.getJSONfromQuery('"you know that there are"',api,3)
     results = [{'timestamp': '2:31:23AM Tue Apr 5 2016', 'handle': '@DepEd_PH', 'text': 'Did you know that there are 21 sports (17 annual events &amp; 4 demonstration sports) in Palarong Pambansa? #Palaro2016 https://t.co/VshYsu1biZ'},
-                {'timestamp': '7:37:02AM Fri Apr 1 2016', 'handle': '@DrRanj', 'text': "Did you know that there are over 175,000 #youngcarers (5-18yrs) in the UK? Join us on @itvthismorning today where we'll be meeting some!"}, {'timestamp': '11:25:34PM Fri Apr 8 2016', 'handle': '@sarahmerie17', 'text': 'You know that there are some people who would never do for you what you do for them.  And you do it anyways.'}]
-    print(results)
+                {'timestamp': '7:37:02AM Fri Apr 1 2016', 'handle': '@DrRanj', 'text': "Did you know that there are over 175,000 #youngcarers (5-18yrs) in the UK? Join us on @itvthismorning today where we'll be meeting some!"},
+                {'timestamp': '11:25:34PM Fri Apr 8 2016', 'handle': '@sarahmerie17', 'text': 'You know that there are some people who would never do for you what you do for them.  And you do it anyways.'}]
+    #print(results)
+    now = datetime.datetime.now().strftime("%I:%M%p")
+    intro = {"handle":"14CodeMonkeys","now":now, "instruct":
+    '''If you want to experience the wisdom of Cat Stevens through live tweets
+    ...click play :)'''}
+    header="If you want to..."
+    foot="Footer"
 
-    return render_template('index.html', data=results, info=info)
-
+    return render_template('index.html', data=results, safeData=json.dumps(results),
+    intro=intro, header=header, foot=foot)
 
 if __name__ == "__main__":
     app.run()
